0ca7fea88ba397ae57b102cd985c3018fcda45ba
Ben Mulholland <ben.mulholland@gmail.com>
2020-07-31 14:41:56 -0400

    added chap 17 to code need to reorder to make work

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+// from chapter 16 of wes bos 30 days of vanilla js
 
   const addItems = document.querySelector('.add-items');
   const itemsList = document.querySelector('.plates');
@@ -44,3 +45,39 @@ itemsList.addEventListener('click', toggleDone);
 
 
 populateList(items, itemsList)
+
+
+// from chapter 17 of wes bos 30 days of vanilla js
+
+
+
+const hero = document.querySelector('.wrapper');
+const text = hero.querySelector('h1');
+const walk = 200; //100px
+
+
+function shadow(e) {
+  //get width and height of thing
+  const { offsetWidth: width, offsetHeight: height } = hero; 
+  let { offsetX: x, offsetY: y } = e;
+
+    if (this !== e.target) {
+      x = x + e.target.offsetLeft;
+      y = y + e.target.offsetTop;
+    }
+
+    const xWalk = Math.round((x / width * walk) - (walk / 2));
+    const yWalk = Math.round((y / height * walk) - (walk / 2));
+    //keeps around 50 and 100 instead of zero
+  // console.log(x, y);
+  // console.log(xWalk, yWalk);
+
+    // text.style.textShadow = `10px 10px 0 red`;
+    text.style.textShadow = `
+      ${xWalk}px ${yWalk}px 0 rgba(255,0,255,0.7),
+      ${xWalk * -1}px ${yWalk}px 0 rgba(0,255,255,0.7),
+      ${yWalk}px ${xWalk * -1}px 0 rgba(0,255,0,0.7),
+      ${yWalk * -1}px ${xWalk}px 0 rgba(0,0,255,0.7)
+    
+    `;
+  }
