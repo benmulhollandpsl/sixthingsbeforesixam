6936e357fc4dd89d9832556e537dd066293beb4f
Ben Mulholland <ben.mulholland@gmail.com>
2020-08-25 11:24:58 -0400

    added new blog entry, added gif of progress, added js of speech entry to try to encorporate into blog

--- README.md
+++ README.md
@@ -5,3 +5,16 @@ a localstorage page with js to store a few things to bedone the nextmorning prio
 created as part of wes box 30 days of js course
 
 https://picsvg.com/ used to turn piskel William into svg then changed inline.
+
+created as part of a modification of units learned in 30 days js course by Wes Bos. 
+
+blog added mid August originally the idea was to make a local storage type of blog, but I might use this as my current blog on a future subpage of my next portfolio
+
+
+
+
+## progress as of 8/25 for blog
+
+![Gif of 8-25 progress](images/Blog for 6 things before 6.gif)
+
+  
--- blog.js
+++ blog.js
@@ -1,6 +1,5 @@
 
 // blog page
-  // maybe switch back to inline and add a menu to six things page
   const nav = document.querySelector('#main');
   const topOfNav = nav.offsetTop;
   
@@ -16,5 +15,54 @@
   
   }
   
+
+  //speech to text function
+  window.SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition;
+
+
+  const recognition = new SpeechRecognition();
+  
+  
+  recognition.interimResults = true;
+  
+  let p = document.createElement('p');
+  const words = document.querySelector('.words');
+  words.appendChild(p);
+
+  recognition.addEventListener('result', e => {
+    console.log(e.results);
+    const transcript = Array.from(e.results)
+      .map(result => result[0])
+      .map(result => result.transcript)
+      .join('')
+
+      p.textContent = transcript;
+      if(e.results[0].isFinal){
+        p = document.createElement('p');
+        words.appendChild(p);
+      }
+
+        if(transcript.includes('unicorn')){
+          console.log('🦄')
+        };
+
+        if(transcript.includes('get the weather')){
+          console.log('Getting the weather')
+          // then run a dang function to get it
+        };
+
+    console.log(transcript);
+  
+  });
+  recognition.addEventListener('end',recognition.start);
+  
+
+recognition.start();
+
+
+
+
+
+
   window.addEventListener('scroll', fixNav);
   
